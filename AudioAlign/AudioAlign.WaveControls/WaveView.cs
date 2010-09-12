734181216166d597a1de53c80aef4bde71e3046b
Mario Guggenberger <mg@protyposis.net>
2010-09-12 19:21:36 +0000

    WaveView virtualized

--- AudioAlign/AudioAlign.WaveControls/WaveView.cs
+++ AudioAlign/AudioAlign.WaveControls/WaveView.cs
@@ -18,8 +18,8 @@ using System.Diagnostics;
 namespace AudioAlign.WaveControls {
     public class WaveView : Control {
 
-        public static readonly DependencyProperty ParentViewportHorizontalOffsetProperty;
-        public static readonly DependencyProperty ParentViewportWidthProperty;
+        public static readonly DependencyProperty VirtualHorizontalOffsetProperty;
+        public static readonly DependencyProperty VirtualWidthProperty;
 
         private const int BUFFER_SIZE = 512;
         private const int BYTES_PER_SAMPLE = 2;
@@ -29,11 +29,11 @@ namespace AudioAlign.WaveControls {
         static WaveView() {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(WaveView), new FrameworkPropertyMetadata(typeof(WaveView)));
 
-            FrameworkPropertyMetadata viewportHorizontalOffsetMetadata = new FrameworkPropertyMetadata() { AffectsRender = true };
-            FrameworkPropertyMetadata viewportWidthMetadata = new FrameworkPropertyMetadata() { AffectsRender = true };
+            FrameworkPropertyMetadata virtualHorizontalOffsetMetadata = new FrameworkPropertyMetadata() { AffectsRender = true };
+            FrameworkPropertyMetadata virtualWidthMetadata = new FrameworkPropertyMetadata() { AffectsRender = true };
 
-            ParentViewportHorizontalOffsetProperty = DependencyProperty.Register("ParentViewportHorizontalOffset", typeof(double), typeof(WaveView), viewportHorizontalOffsetMetadata);
-            ParentViewportWidthProperty = DependencyProperty.Register("ParentViewportWidth", typeof(double), typeof(WaveView), viewportWidthMetadata);
+            VirtualHorizontalOffsetProperty = DependencyProperty.Register("VirtualHorizontalOffset", typeof(double), typeof(WaveView), virtualHorizontalOffsetMetadata);
+            VirtualWidthProperty = DependencyProperty.Register("VirtualWidth", typeof(double), typeof(WaveView), virtualWidthMetadata);
         }
 
         private WaveStream waveStream;
@@ -47,18 +47,18 @@ namespace AudioAlign.WaveControls {
             get { return waveStream; }
             set { 
                 waveStream = value;
-                Width = waveStream.Length / 2;
+                VirtualWidth = waveStream.Length / 2;
             }
         }
 
-        public double ParentViewportHorizontalOffset {
-            get { return (double)GetValue(ParentViewportHorizontalOffsetProperty); }
-            set { SetValue(ParentViewportHorizontalOffsetProperty, value); }
+        public double VirtualHorizontalOffset {
+            get { return (double)GetValue(VirtualHorizontalOffsetProperty); }
+            set { SetValue(VirtualHorizontalOffsetProperty, value); }
         }
 
-        public double ParentViewportWidth {
-            get { return (double)GetValue(ParentViewportWidthProperty); }
-            set { SetValue(ParentViewportWidthProperty, value); }
+        public double VirtualWidth {
+            get { return (double)GetValue(VirtualWidthProperty); }
+            set { SetValue(VirtualWidthProperty, value); }
         }
 
         protected override void OnRender(DrawingContext drawingContext) {
@@ -69,29 +69,29 @@ namespace AudioAlign.WaveControls {
 
             if (waveStream != null) {
                 long offset = (long)Math.Floor(viewport.Left);
-                int width = (int)Math.Ceiling(viewport.Width) + 1;
+                int width = (int)Math.Ceiling(ActualWidth) + 1; // +1 to avoid small gap on right border of graph (draws the graph over the right border)
                 drawingContext.DrawGeometry(Brushes.Red, new Pen(Brushes.Black, 1), CreateWaveform(offset, width));
             }
 
             // DEBUG OUTPUT: VIEWPORT
+            String viewportInfo = "Size: " + new Size(ActualWidth, ActualHeight) + " / " + "Viewport: " + viewport.ToString();
             drawingContext.DrawText(
                 new FormattedText(
-                    "Viewport: " + viewport.ToString(),
+                    viewportInfo,
                     CultureInfo.CurrentUICulture,
                     System.Windows.FlowDirection.LeftToRight,
                     new Typeface("Tahoma"),
                     8,
                     Brushes.Black),
-                viewport.BottomLeft + new Vector(0, -10));
+                new Point(0, ActualHeight) + new Vector(0, -10));
         }
 
         private Rect CalculateViewport() {
-            return new Rect(ParentViewportHorizontalOffset, 0, ParentViewportWidth, ActualHeight);
+            return new Rect(VirtualHorizontalOffset, 0, ActualWidth, ActualHeight);
         }
 
         private void PaintWaveformBackground(Rect viewport, DrawingContext drawingContext) {
-            drawingContext.DrawLine(new Pen(Brushes.Gray, 1), new Point(viewport.Left, viewport.Top + viewport.Height / 2), new Point(viewport.Right, viewport.Top + viewport.Height / 2));
-            drawingContext.DrawLine(new Pen(Brushes.Red, 3), viewport.TopLeft, viewport.BottomLeft);
+            drawingContext.DrawLine(new Pen(Brushes.Gray, 1), new Point(0, viewport.Height / 2), new Point(ActualWidth, viewport.Height / 2));
         }
 
         private Geometry CreateWaveform(long offset, int samples) {
@@ -107,14 +107,14 @@ namespace AudioAlign.WaveControls {
 
                 for (int x = 0; x < bytesRead; x += BYTES_PER_SAMPLE * 2) {
                     short sample = (short)((buffer[x + 1] << 8) | buffer[x + 0]);
-                    linePoints.Add(new Point(offset + samplesRead, (height / 2) - (sample * horizontalScale)));
+                    linePoints.Add(new Point(samplesRead, (height / 2) - (sample * horizontalScale)));
                     samplesRead++;
                     if (samplesRead == samples) break;
                 }
             }
 
             if (samplesRead == 0) {
-                throw new NotImplementedException();
+                return new PathGeometry();
             }
             else if (samplesRead == 1) {
                 throw new NotImplementedException();
