6cde3fe68fb59713ce2466d4efa45ec80ec55ace
Mario Guggenberger <mg@protyposis.net>
2010-11-21 14:22:07 +0000

    * FEATURE ResizeDecorator reset height by thumb doubleclick

--- AudioAlign/AudioAlign.WaveControls/ResizeDecorator.cs
+++ AudioAlign/AudioAlign.WaveControls/ResizeDecorator.cs
@@ -24,15 +24,21 @@ namespace AudioAlign.WaveControls {
         private void ResizeDecorator_Loaded(object sender, RoutedEventArgs e) {
             Thumb resizeThumb = GetTemplateChild("PART_ResizeThumb") as Thumb;
             if (resizeThumb != null) {
-                resizeThumb.DragDelta += new DragDeltaEventHandler(thumb_DragDelta);
+                resizeThumb.DragDelta += new DragDeltaEventHandler(resizeThumb_DragDelta);
+                resizeThumb.MouseDoubleClick += new System.Windows.Input.MouseButtonEventHandler(resizeThumb_MouseDoubleClick);
             }
         }
 
-        private void thumb_DragDelta(object sender, DragDeltaEventArgs e) {
+        private void resizeThumb_DragDelta(object sender, DragDeltaEventArgs e) {
             Debug.WriteLine("ResizeDecorator ResizeThumb DragDelta: " + e.VerticalChange);
 
             double newHeight = Math.Max(MinHeight, ActualHeight + e.VerticalChange);
             SetValue(HeightProperty, newHeight);
+            e.Handled = true;
+        }
+
+        void resizeThumb_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            ClearValue(HeightProperty);
             e.Handled = true;
         }
 
