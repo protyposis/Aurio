f6bded48eb4ca669c88c06ef76b00a5c7af85163
Mario Guggenberger <mg@protyposis.net>
2011-01-08 17:53:23 +0000

    * FEATURE NAudio VolumeMeteringStream * FEATURE VU-Meter control * FEATURE multitrack playback test app

---
+++ AudioAlign/AudioAlign.Test.MultitrackPlayback/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AudioAlign.Audio.Project;
+using System.IO;
+using NAudio.Wave;
+using AudioAlign.Audio.NAudio;
+using System.Timers;
+using System.Windows.Interop;
+using System.Windows.Threading;
+
+namespace AudioAlign.Test.MultitrackPlayback {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private Timer timer;
+        private IWavePlayer wavePlayer;
+        private WaveStream playbackStream;
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnAddFile_Click(object sender, RoutedEventArgs e) {
+            // http://msdn.microsoft.com/en-us/library/aa969773.aspx
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "audio"; // Default file name
+            dlg.DefaultExt = ".wav"; // Default file extension
+            dlg.Filter = "Wave files (.wav)|*.wav"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true) {
+                // Open document
+                AudioTrack audioTrack = new AudioTrack(new FileInfo(dlg.FileName));
+                //audioTrack.Offset = new TimeSpan(new Random().Next((int)new TimeSpan(0, 10, 0).Ticks));
+                trackListBox.Items.Add(audioTrack);
+            }
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e) {
+            //List<AudioTrack> tracks = new List<AudioTrack>();
+            //tracks.AddRange(trackListBox.Items.Cast<AudioTrack>());
+
+            WaveMixerStream32 mixer = new WaveMixerStream32();
+            foreach (AudioTrack audioTrack in trackListBox.Items) {
+                WaveFileReader reader = new WaveFileReader(audioTrack.FileInfo.FullName);
+                WaveChannel32 channel = new WaveChannel32(reader);
+                mixer.AddInputStream(channel);
+            }
+
+            VolumeMeteringStream meteringStream = new VolumeMeteringStream(mixer);
+            meteringStream.StreamVolume += new EventHandler<StreamVolumeEventArgs>(meteringStream_StreamVolume);
+
+            playbackStream = meteringStream;
+
+            wavePlayer = new WaveOut();
+            wavePlayer.Init(playbackStream);
+
+            wavePlayer.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e) {
+            if (wavePlayer.PlaybackState == PlaybackState.Paused) {
+                wavePlayer.Play();
+            }
+            else {
+                wavePlayer.Pause();
+            }
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e) {
+            wavePlayer.Stop();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            timer = new Timer(50);
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.Enabled = true;
+        }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            if (playbackStream != null) {
+                playbackStream.Close();
+            }
+
+            if (wavePlayer != null) {
+                wavePlayer.Dispose();
+            }
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e) {
+            if (wavePlayer != null) {
+                lblCurrentPlaybackTime.Dispatcher.BeginInvoke(DispatcherPriority.Normal, 
+                    new DispatcherOperationCallback(delegate {
+                        lblCurrentPlaybackTime.Content = playbackStream.CurrentTime.ToString();
+                        return null;
+                   }), null);
+            }
+        }
+
+        private void meteringStream_StreamVolume(object sender, StreamVolumeEventArgs e) {
+            if (e.MaxSampleValues.Length >= 2) {
+                vUMeterCh1.Amplitude = e.MaxSampleValues[0];
+                vUMeterCh2.Amplitude = e.MaxSampleValues[1];
+            }
+        }
+    }
+}
