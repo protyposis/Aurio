5e513a3347ea9615b52f78ca427a165d6e223e19
Mario Guggenberger <mg@protyposis.net>
2011-08-23 19:45:17 +0000

    * FEATURE basic sync quality analysis

---
+++ AudioAlign/AudioAlign.Audio/Matching/Analysis.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AudioAlign.Audio.Project;
+using AudioAlign.Audio.Streams;
+using System.Diagnostics;
+
+namespace AudioAlign.Audio.Matching {
+    public class Analysis {
+
+        public static void Analyze(TrackList<AudioTrack> audioTracks, TimeSpan windowLength, TimeSpan intervalLength) {
+            if (audioTracks.Count < 2) {
+                // there must be at least 2 tracks, otherwise there's nothing to compare
+                return;
+            }
+            if (intervalLength < windowLength) {
+                throw new ArgumentException("intervalLength must be at least as long as the windowLength");
+            }
+
+            List<IAudioStream> streams = new List<IAudioStream>(audioTracks.Count);
+            TimeSpan start = TimeSpan.MaxValue;
+            TimeSpan end = TimeSpan.MinValue;
+            foreach (AudioTrack audioTrack in audioTracks) {
+                streams.Add(CrossCorrelation.PrepareStream(audioTrack.CreateAudioStream(), 11050));
+                if (audioTrack.Offset < start) {
+                    start = audioTrack.Offset;
+                }
+                if (audioTrack.Offset + audioTrack.Length > end) {
+                    end = audioTrack.Offset + audioTrack.Length;
+                }
+            }
+
+            long[] streamOffsets = new long[audioTracks.Count];
+            for (int i = 0; i < audioTracks.Count; i++) {
+                streamOffsets[i] = TimeUtil.TimeSpanToBytes(audioTracks[i].Offset - start, streams[0].Properties);
+            }
+
+            int windowLengthInBytes = (int)TimeUtil.TimeSpanToBytes(windowLength, streams[0].Properties);
+            int windowLengthInSamples = windowLengthInBytes / streams[0].Properties.SampleBlockByteSize;
+            long intervalLengthInBytes = TimeUtil.TimeSpanToBytes(intervalLength, streams[0].Properties);
+            long analysisIntervalLength = TimeUtil.TimeSpanToBytes(end - start, streams[0].Properties);
+
+            byte[] x = new byte[windowLengthInBytes];
+            byte[] y = new byte[windowLengthInBytes];
+            long positionX;
+            long positionY;
+            float sumNegative = 0;
+            float sumPositive = 0;
+            unsafe {
+                fixed (byte* xB = &x[0], yB = &y[0]) {
+                    float* xF = (float*)xB;
+                    float* yF = (float*)yB;
+                    for (long position = 0; position < analysisIntervalLength; position += intervalLengthInBytes) {
+                        Debug.WriteLine("Analyzing {0} / {1}", position, analysisIntervalLength);
+                        // at each position in the analysis interval, compare each stream with each other
+                        for (int i = 0; i < streams.Count; i++) {
+                            positionX = position - streamOffsets[i];
+                            if (positionX >= 0 && positionX < streams[i].Length) {
+                                streams[i].Position = positionX;
+                                StreamUtil.ForceRead(streams[i], x, 0, windowLengthInBytes);
+                                for (int j = i + 1; j < streams.Count; j++) {
+                                    positionY = position - streamOffsets[j];
+                                    if (positionY >= 0 && positionY < streams[j].Length) {
+                                        streams[j].Position = positionY;
+                                        StreamUtil.ForceRead(streams[j], y, 0, windowLengthInBytes);
+
+                                        float val = CrossCorrelation.Correlate(xF, yF, windowLengthInSamples);
+                                        if (val > 0) {
+                                            sumPositive += val;
+                                        }
+                                        else {
+                                            sumNegative += val;
+                                        }
+                                        Debug.WriteLine("{0,2}->{1,2}: {2}", i, j, val);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Debug.WriteLine("Finished. sum: {0}, sum+: {1}, sum-: {2}, sumAbs: {3}", 
+                sumPositive + sumNegative, sumPositive, sumNegative, sumPositive + (sumNegative * -1));
+        }
+    }
+}
--- AudioAlign/AudioAlign.Audio/Matching/CrossCorrelation.cs
+++ AudioAlign/AudioAlign.Audio/Matching/CrossCorrelation.cs
@@ -15,27 +15,9 @@ namespace AudioAlign.Audio.Matching {
             if (i1.Length != i2.Length) {
                 throw new ArgumentException("interval lengths do not match");
             }
-            if (s1.Properties.SampleRate != s2.Properties.SampleRate) {
-                throw new ArgumentException("sample rates do not match");
-            }
-            if (s1.Properties.BitDepth != s2.Properties.BitDepth) {
-                throw new ArgumentException("sample bitdepths do not match");
-            }
-            if (s1.Properties.Channels > 1) {
-                if (s1.Properties.Format != AudioFormat.IEEE) {
-                    s1 = new IeeeStream(s1);
-                }
-                s1 = new MonoStream(s1);
-            }
-            if (s2.Properties.Channels > 1) {
-                if (s2.Properties.Format != AudioFormat.IEEE) {
-                    s2 = new IeeeStream(s2);
-                }
-                s2 = new MonoStream(s2);
-            }
 
-            s1 = new ResamplingStream(s1, ResamplingQuality.SincFastest, 11050);
-            s2 = new ResamplingStream(s2, ResamplingQuality.SincFastest, 11050);
+            s1 = PrepareStream(s1, 11050);
+            s2 = PrepareStream(s2, 11050);
 
             ProgressReporter progress = ProgressMonitor.Instance.BeginTask("calculating cross-correlation", true);
 
@@ -116,7 +98,17 @@ namespace AudioAlign.Audio.Matching {
                 }
             }
 
-            Debug.WriteLine("max val: {0} index: {1} adjusted index: {2}", maxval, maxindex, maxindex - maxdelay);
+            float test = Correlate(s1, i1, s2, i2);
+            //float test = Correlate(x, y);
+            Debug.WriteLine("correlation = {0}, r[zero] = {1}", test, r[maxdelay]);
+            for (int i = 0; i < r.Length; i++) {
+                if (r[i] == test) {
+                    Debug.WriteLine("!!!!!! " + i);
+                    break;
+                }
+            }
+
+                Debug.WriteLine("max val: {0} index: {1} adjusted index: {2}", maxval, maxindex, maxindex - maxdelay);
             TimeSpan offset = new TimeSpan((long)((maxindex - maxdelay) / (float)sampleRate * 1000 * 1000 * 10));
             Debug.WriteLine("peak offset @ " + offset);
             ProgressMonitor.Instance.EndTask(progress);
@@ -139,6 +131,96 @@ namespace AudioAlign.Audio.Matching {
                 match.Track2.CreateAudioStream(), new Interval(match.Track2Time.Ticks - intervalLength / 2, match.Track2Time.Ticks + intervalLength / 2));
             Debug.WriteLine("CC: " + match + ": " + offset + " (" + sw.Elapsed + ")");
             match.Track2.Offset -= offset;
+        }
+
+        public static unsafe float Correlate(float* x, float* y, int length) {
+            int n = length;
+
+            // the following code is taken and adapted from: http://local.wasp.uwa.edu.au/~pbourke/miscellaneous/correlate/
+
+            /* Calculate the mean of the two series x[], y[] */
+            double mx = 0;
+            double my = 0;
+            for (int i = 0; i < n; i++) {
+                mx += x[i];
+                my += y[i];
+            }
+            mx /= n;
+            my /= n;
+
+            /* Calculate the denominator */
+            double sx = 0;
+            double sy = 0;
+            for (int i = 0; i < n; i++) {
+                sx += (x[i] - mx) * (x[i] - mx);
+                sy += (y[i] - my) * (y[i] - my);
+            }
+            double denom = Math.Sqrt(sx * sy);
+
+            /* Calculate the correlation */
+            double sxy = 0;
+            for (int i = 0; i < n; i++) {
+                sxy += (x[i] - mx) * (y[i] - my);
+            }
+            
+            return (float)(sxy / denom);
+        }
+
+        public static unsafe float Correlate(float[] x, float[] y) {
+            if (x.Length != y.Length) {
+                throw new ArgumentException("interval lengths do not match");
+            }
+            fixed(float* xF = &x[0], yF = &y[0]) {
+                return Correlate(xF, yF, x.Length / sizeof(float));
+            }
+        }
+
+        public static float Correlate(IAudioStream s1, Interval i1, IAudioStream s2, Interval i2) {
+            if (i1.Length != i2.Length) {
+                throw new ArgumentException("interval lengths do not match");
+            }
+
+            s1 = PrepareStream(s1, 11050);
+            s2 = PrepareStream(s2, 11050);
+
+            ProgressReporter progress = ProgressMonitor.Instance.BeginTask("calculating correlation", true);
+
+            float seconds = (float)(i1.Length / 10d / 1000 / 1000);
+            int sampleRate = s1.Properties.SampleRate;
+            int n = (int)(seconds * sampleRate);
+            float[] x = new float[n];
+            float[] y = new float[n];
+
+            Debug.WriteLine("C window: " + seconds + " secs = " + n + " samples");
+
+            DateTime timeBeforeDataRead = DateTime.Now;
+
+            StreamUtil.ForceReadIntervalSamples(s1, i1, x);
+            StreamUtil.ForceReadIntervalSamples(s2, i2, y);
+
+            TimeSpan timeOfDataRead = DateTime.Now - timeBeforeDataRead;
+            Debug.WriteLine("C data read duration: " + timeOfDataRead);
+
+            float r = Correlate(x, y);
+
+            progress.ReportProgress(100);
+
+            Debug.WriteLine("C result: " + r);
+            ProgressMonitor.Instance.EndTask(progress);
+            return r;
+        }
+
+        public static IAudioStream PrepareStream(IAudioStream stream, int sampleRate) {
+            if (stream.Properties.Format != AudioFormat.IEEE) {
+                stream = new IeeeStream(stream);
+            }
+            if (stream.Properties.Channels > 1) {
+                stream = new MonoStream(stream);
+            }
+            if (stream.Properties.SampleRate != 11050) {
+                stream = new ResamplingStream(stream, ResamplingQuality.SincFastest, sampleRate);
+            }
+            return stream;
         }
     }
 }
