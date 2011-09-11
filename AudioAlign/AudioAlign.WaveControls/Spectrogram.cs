ed5fb058b45e1f403c103c05847778e11d4a176c
Mario Guggenberger <mg@protyposis.net>
2011-09-11 12:17:41 +0000

    * FEATURE Spectrogram scroll mode

--- AudioAlign/AudioAlign.WaveControls/Spectrogram.cs
+++ AudioAlign/AudioAlign.WaveControls/Spectrogram.cs
@@ -11,11 +11,32 @@ using System.Windows;
 namespace AudioAlign.WaveControls {
     public class Spectrogram : Control {
 
-        private WriteableBitmap writableBitmap;
+        /// <summary>
+        /// Specified how much times the scroll mode bitmap should be larger than the actual control's width.
+        /// The bigger it is, the more memory is consumed, but the less bitmap copy operations need to be executed.
+        /// </summary>
+        private const int SCROLL_WIDTH_FACTOR = 3;
+
+        private WriteableBitmap writeableBitmap;
         private int position;
         private int[] pixelColumn;
+        private SpectrogramMode mode;
 
         private int[] colorPalette;
+
+        public SpectrogramMode Mode {
+            get { return (SpectrogramMode)GetValue(ModeProperty); }
+            set { SetValue(ModeProperty, value); }
+        }
+
+        public static readonly DependencyProperty ModeProperty =
+            DependencyProperty.Register("Mode", typeof(SpectrogramMode), typeof(Spectrogram),
+            new UIPropertyMetadata(SpectrogramMode.Scroll, OnModeChanged));
+
+        private static void OnModeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            Spectrogram spectrogram = d as Spectrogram;
+            spectrogram.mode = (SpectrogramMode)e.NewValue;
+        }
 
         public int SpectrogramSize {
             get { return (int)GetValue(SpectrogramSizeProperty); }
@@ -36,7 +57,7 @@ namespace AudioAlign.WaveControls {
 
         private static void OnSpectrogramSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
             Spectrogram spectrogram = d as Spectrogram;
-            spectrogram.InitializeSpectrogramBitmap();
+            spectrogram.InitializeSpectrogramBitmap(true);
         }
 
         public float Minimum {
@@ -58,24 +79,34 @@ namespace AudioAlign.WaveControls {
         public Spectrogram() {
             ColorGradient gradient = new ColorGradient(0, 1);
             gradient.AddStop(Colors.Black, 0);
-            gradient.AddStop(Colors.White, 1);
-            colorPalette = gradient.GetGradient(256).Select(c => GetColorValue(c)).ToArray();
+            gradient.AddStop(Colors.DarkBlue, 0.3f);
+            gradient.AddStop(Colors.DarkOrange, 0.85f);
+            gradient.AddStop(Colors.Yellow, 1);
+            colorPalette = gradient.GetGradient(1024).Select(c => GetColorValue(c)).ToArray();
+
+            ClipToBounds = true;
+            mode = Mode;
         }
 
         protected override void OnRender(System.Windows.Media.DrawingContext drawingContext) {
             base.OnRender(drawingContext);
-
             drawingContext.DrawRectangle(Background, null, new Rect(0, 0, ActualWidth, ActualHeight));
 
-            ImageDrawing drawing = new ImageDrawing(writableBitmap, new Rect(0, 0, ActualWidth, ActualHeight));
-            drawingContext.DrawDrawing(drawing);
+            if (mode == SpectrogramMode.Scroll) {
+                drawingContext.DrawDrawing(new ImageDrawing(
+                    writeableBitmap, new Rect(ActualWidth - position, 0, ActualWidth * 3, ActualHeight)));
+            }
+            else {
+                drawingContext.DrawDrawing(new ImageDrawing(
+                    writeableBitmap, new Rect(0, 0, ActualWidth, ActualHeight)));
+            }
         }
 
         protected override void OnRenderSizeChanged(System.Windows.SizeChangedInfo sizeInfo) {
             base.OnRenderSizeChanged(sizeInfo);
 
             // recreate bitmap for current control size
-            InitializeSpectrogramBitmap();
+            InitializeSpectrogramBitmap(true);
         }
 
         public void AddSpectrogramColumn(float[] values) {
@@ -84,7 +115,7 @@ namespace AudioAlign.WaveControls {
                     + " but is " + values.Length);
             }
 
-            float factor = 255f / (Maximum - Minimum);
+            float factor = 1024f / (Maximum - Minimum);
             for (int x = 0; x < pixelColumn.Length; x++) {
                 float value = values[x];
                 int color;
@@ -95,30 +126,71 @@ namespace AudioAlign.WaveControls {
                     color = colorPalette[0];
                 }
                 else {
-                    color = colorPalette[255 + (int)(factor * values[x])];
+                    color = colorPalette[1024 + (int)(factor * values[x])];
                 }
                 pixelColumn[pixelColumn.Length - 1 - x] = color;
             }
 
-            writableBitmap.WritePixels(new Int32Rect(position, 0, 1, values.Length), pixelColumn, 4, 0);
+            writeableBitmap.WritePixels(new Int32Rect(position, 0, 1, values.Length), pixelColumn, 4, 0);
 
-            if (++position >= writableBitmap.PixelWidth) {
-                position = 0;
+            if (++position >= writeableBitmap.PixelWidth) {
+                if (mode == SpectrogramMode.Scroll) {
+                    position = (int)ActualWidth;
+                    InitializeSpectrogramBitmap(false);
+                }
+                else {
+                    position = 0;
+                }
             }
 
             InvalidateVisual();
         }
 
-        private void InitializeSpectrogramBitmap() {
-            writableBitmap = new WriteableBitmap(
-                (int)ActualWidth, SpectrogramSize, 96, 96, PixelFormats.Bgra32, null);
-            pixelColumn = new int[SpectrogramSize];
-            position = 0;
+        private void InitializeSpectrogramBitmap(bool sizeChanged) {
+            if (writeableBitmap == null) { // first time initialization
+                writeableBitmap = new WriteableBitmap(
+                    mode == SpectrogramMode.Scroll ? (int)ActualWidth * SCROLL_WIDTH_FACTOR : (int)ActualWidth,
+                    SpectrogramSize, 96, 96, PixelFormats.Bgra32, null);
+                pixelColumn = new int[SpectrogramSize];
+            }
+            else {
+                if (pixelColumn.Length != SpectrogramSize) {
+                    pixelColumn = new int[SpectrogramSize];
+                }
+                if (mode == SpectrogramMode.Scroll) {
+                    if (sizeChanged) {
+                        writeableBitmap = new WriteableBitmap((int)ActualWidth * SCROLL_WIDTH_FACTOR,
+                            SpectrogramSize, 96, 96, PixelFormats.Bgra32, null);
+                    }
+                    else {
+                        CopyPixels(writeableBitmap, writeableBitmap);
+                    }
+                }
+                else if (sizeChanged) {
+                    writeableBitmap = new WriteableBitmap((int)ActualWidth,
+                            SpectrogramSize, 96, 96, PixelFormats.Bgra32, null);
+                }
+            }
+            
+            position = mode == SpectrogramMode.Scroll ? (int)ActualWidth : 0;
             InvalidateVisual();
         }
 
         private static int GetColorValue(Color c) {
             return c.A << 24 | c.R << 16 | c.G << 8 | c.B;
+        }
+
+        private static void CopyPixels(WriteableBitmap src, WriteableBitmap dest) {
+            int twoThirds = src.PixelWidth / 3 * 2;
+            int third = src.PixelWidth / 3;
+            int height = src.PixelHeight;
+
+            Int32Rect srcRect = new Int32Rect(twoThirds, 0, third, height);
+            Int32Rect destRect = new Int32Rect(0, 0, third, height);
+
+            int[] buffer = new int[twoThirds * height];
+            src.CopyPixels(srcRect, buffer, third * 4, 0);
+            dest.WritePixels(destRect, buffer, third * 4, 0);
         }
     }
 }
---
+++ AudioAlign/AudioAlign.WaveControls/SpectrogramMode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AudioAlign.WaveControls {
+    public enum SpectrogramMode {
+        Static,
+        Scroll
+    }
+}
