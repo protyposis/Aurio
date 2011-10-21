f41bb0ec73fa3675ed07fcb40acda27ca9128c20
Mario Guggenberger <mg@protyposis.net>
2011-10-21 17:52:37 +0000

    * IMPROVEMENT DTW enhanced for parallel processing (no more initial reading of whole streams necessary)

--- AudioAlign/AudioAlign.Audio/Matching/Dixon2005/DTW.cs
+++ AudioAlign/AudioAlign.Audio/Matching/Dixon2005/DTW.cs
@@ -6,6 +6,7 @@ using AudioAlign.Audio.Streams;
 using AudioAlign.Audio.TaskMonitor;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace AudioAlign.Audio.Matching.Dixon2005 {
     public class DTW {
@@ -23,6 +24,14 @@ namespace AudioAlign.Audio.Matching.Dixon2005 {
         protected ProgressMonitor progressMonitor;
         private TimeSpan maxOffset;
 
+        private BlockingCollection<float[]> stream1FrameQueue;
+        private BlockingCollection<float[]> stream2FrameQueue;
+        private int diagonalWidth;
+        float[][] rb1;
+        private int rb1FrameCount;
+        float[][] rb2;
+        private int rb2FrameCount;
+
         public DTW(TimeSpan maxOffset, ProgressMonitor progressMonitor) {
             this.maxOffset = maxOffset;
             this.progressMonitor = progressMonitor;
@@ -32,67 +41,39 @@ namespace AudioAlign.Audio.Matching.Dixon2005 {
             s1 = PrepareStream(s1);
             s2 = PrepareStream(s2);
 
-            float[][] frames1 = null;
-            float[][] frames2 = null;
+            diagonalWidth = (int)(maxOffset.TotalSeconds * (1d * FrameReader.SAMPLERATE / FrameReader.WINDOW_HOP_SIZE));
 
-            Parallel.For(0, 2, index => {
-                if (index == 0) {
-                    frames1 = ReadFrames(s1);
+            // init ring buffers
+            rb1 = new float[diagonalWidth][];
+            rb1FrameCount = 0;
+            rb2 = new float[diagonalWidth][];
+            rb2FrameCount = 0;
+
+            stream1FrameQueue = new BlockingCollection<float[]>(20);
+            FrameReader stream1FrameReader = new FrameReader(s1);
+            Task.Factory.StartNew(() => {
+                while (stream1FrameReader.HasNext()) {
+                    float[] frame = new float[FrameReader.FRAME_SIZE];
+                    stream1FrameReader.ReadFrame(frame);
+                    stream1FrameQueue.Add(frame);
                 }
-                else if (index == 1) {
-                    frames2 = ReadFrames(s2);
-                }
+                stream1FrameQueue.CompleteAdding();
             });
 
-            PatchMatrix dtw = AccumulatedCostMatrix(frames1, frames2);
-            List<Pair> path = OptimalWarpingPath(dtw);
-
-            List<Tuple<TimeSpan, TimeSpan>> pathTimes = new List<Tuple<TimeSpan, TimeSpan>>();
-            foreach (Pair pair in path) {
-                Tuple<TimeSpan, TimeSpan> timePair = new Tuple<TimeSpan, TimeSpan>(
-                    PositionToTimeSpan(pair.i1 * FrameReader.WINDOW_HOP_SIZE),
-                    PositionToTimeSpan(pair.i2 * FrameReader.WINDOW_HOP_SIZE));
-                if (timePair.Item1 >= TimeSpan.Zero && timePair.Item2 >= TimeSpan.Zero) {
-                    pathTimes.Add(timePair);
+            stream2FrameQueue = new BlockingCollection<float[]>(20);
+            FrameReader stream2FrameReader = new FrameReader(s2);
+            Task.Factory.StartNew(() => {
+                while (stream2FrameReader.HasNext()) {
+                    float[] frame = new float[FrameReader.FRAME_SIZE];
+                    stream2FrameReader.ReadFrame(frame);
+                    stream2FrameQueue.Add(frame);
                 }
-            }
+                stream2FrameQueue.CompleteAdding();
+            });
 
-            return pathTimes;
-        }
-
-        protected IAudioStream PrepareStream(IAudioStream stream) {
-            if (stream.Properties.Channels > 1) {
-                stream = new MonoStream(stream);
-            }
-            if (stream.Properties.SampleRate != FrameReader.SAMPLERATE) {
-                stream = new ResamplingStream(stream, ResamplingQuality.SincFastest, FrameReader.SAMPLERATE);
-            }
-            return stream;
-        }
-
-        private float[][] ReadFrames(IAudioStream stream) {
-            IProgressReporter progressReporter = progressMonitor.BeginTask("Reading DTW frames...", true);
-            float[][] frames = new float[(stream.Length / stream.SampleBlockSize - FrameReader.WINDOW_SIZE) /
-                FrameReader.WINDOW_HOP_SIZE + 1][];
-            FrameReader frameReader = new FrameReader(stream);
-            int index = 0;
-            while (frameReader.HasNext()) {
-                float[] frame = new float[FrameReader.FRAME_SIZE];
-                frameReader.ReadFrame(frame);
-                frames[index++] = frame;
-                progressReporter.ReportProgress((double)index / frames.Length * 100);
-            }
-            progressReporter.Finish();
-            if (index < frames.Length) {
-                Debug.WriteLine("frames array too large!!!");
-            }
-            return frames;
-        }
-
-        private PatchMatrix AccumulatedCostMatrix(float[][] X, float[][] Y) {
-            IProgressReporter progressReporter = progressMonitor.BeginTask("Calculating cost matrix...", true);
-            int n = X.Length;
-            int m = Y.Length;
+            IProgressReporter progressReporter = progressMonitor.BeginTask("DTW...", true);
+            int n = stream1FrameReader.WindowCount;
+            int m = stream2FrameReader.WindowCount;
             PatchMatrix dtw = new PatchMatrix(double.PositiveInfinity);
 
             // init matrix
@@ -100,9 +81,6 @@ namespace AudioAlign.Audio.Matching.Dixon2005 {
             //      be gone and the matrix would take up all the space like a standard matrix does
             dtw[0, 0] = 0;
 
-            double sakoeChibaBandWidth = maxOffset.TotalSeconds * (1d * FrameReader.SAMPLERATE / FrameReader.WINDOW_HOP_SIZE);
-            // Kathetenlänge berechnen (Sakoe-Chiba-Bandbreite ist die Hypothenuse des gleichschenkligen rechtwinkligen Dreiecks)
-            int diagonalWidth = (int)(Math.Sqrt(2) / 2 * sakoeChibaBandWidth);
             double deltaN;
             double deltaM;
             if (m > n) {
@@ -119,24 +97,25 @@ namespace AudioAlign.Audio.Matching.Dixon2005 {
             }
             double progressN = 0;
             double progressM = 0;
-            int x = 0;
-            int y = 0;
+            int i, x = 0;
+            int j, y = 0;
             while (x < n || y < m) {
                 x = (int)progressN + 1;
                 y = (int)progressM + 1;
+                ReadFrames(x, y);
 
-                Debug.WriteLine(x + " / " + y);
-
-                int i = x;
-                int j = y;
-                for (i = x; i <= Math.Min(x + diagonalWidth, n); i++) {
-                    dtw[i, j] = CalculateCost(X[i - 1], Y[j - 1]) + Min(dtw[i - 1, j], dtw[i, j - 1], dtw[i - 1, j - 1]);
+                i = Math.Max(x - diagonalWidth, 1);
+                j = y;
+                for (; i <= x; i++) {
+                    dtw[i, j] = CalculateCost(rb1[(i - 1) % diagonalWidth], rb2[(j - 1) % diagonalWidth]) + 
+                        Min(dtw[i - 1, j], dtw[i, j - 1], dtw[i - 1, j - 1]);
                 }
 
                 i = x;
-                j = y;
-                for (j = y; j <= Math.Min(y + diagonalWidth, m); j++) {
-                    dtw[i, j] = CalculateCost(X[i - 1], Y[j - 1]) + Min(dtw[i - 1, j], dtw[i, j - 1], dtw[i - 1, j - 1]);
+                j = Math.Max(y - diagonalWidth, 1);
+                for (; j <= y; j++) {
+                    dtw[i, j] = CalculateCost(rb1[(i - 1) % diagonalWidth], rb2[(j - 1) % diagonalWidth]) + 
+                        Min(dtw[i - 1, j], dtw[i, j - 1], dtw[i - 1, j - 1]);
                 }
 
                 progressReporter.ReportProgress((double)x / n * 100);
@@ -144,9 +123,53 @@ namespace AudioAlign.Audio.Matching.Dixon2005 {
                 progressN += deltaN;
                 progressM += deltaM;
             }
-
             progressReporter.Finish();
-            return dtw;
+
+            List<Pair> path = OptimalWarpingPath(dtw);
+
+            List<Tuple<TimeSpan, TimeSpan>> pathTimes = new List<Tuple<TimeSpan, TimeSpan>>();
+            foreach (Pair pair in path) {
+                Tuple<TimeSpan, TimeSpan> timePair = new Tuple<TimeSpan, TimeSpan>(
+                    PositionToTimeSpan(pair.i1 * FrameReader.WINDOW_HOP_SIZE),
+                    PositionToTimeSpan(pair.i2 * FrameReader.WINDOW_HOP_SIZE));
+                if (timePair.Item1 >= TimeSpan.Zero && timePair.Item2 >= TimeSpan.Zero) {
+                    pathTimes.Add(timePair);
+                }
+            }
+
+            return pathTimes;
+        }
+
+        private void ReadFrames(int x, int y) {
+            if (rb1FrameCount < x) {
+                for (; rb1FrameCount < x; rb1FrameCount++) {
+                    if (stream1FrameQueue.IsCompleted) {
+                        rb1FrameCount--;
+                        break;
+                    }
+                    rb1[rb1FrameCount % diagonalWidth] = (stream1FrameQueue.Take());
+                }
+            }
+
+            if (rb2FrameCount < y) {
+                for (; rb2FrameCount < y; rb2FrameCount++) {
+                    if (stream2FrameQueue.IsCompleted) {
+                        rb2FrameCount--;
+                        break;
+                    }
+                    rb2[rb2FrameCount % diagonalWidth] = (stream2FrameQueue.Take());
+                }
+            }
+        }
+
+        protected IAudioStream PrepareStream(IAudioStream stream) {
+            if (stream.Properties.Channels > 1) {
+                stream = new MonoStream(stream);
+            }
+            if (stream.Properties.SampleRate != FrameReader.SAMPLERATE) {
+                stream = new ResamplingStream(stream, ResamplingQuality.SincFastest, FrameReader.SAMPLERATE);
+            }
+            return stream;
         }
 
         protected List<Pair> OptimalWarpingPath(PatchMatrix dtw, int i, int j) {
