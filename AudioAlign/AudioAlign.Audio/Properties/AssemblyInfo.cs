11d17387ceda3e5d48fed7e5c4a4bd8d8bb48fb3
Mario Guggenberger <mg@protyposis.net>
2011-10-27 12:35:29 +0000

    * AssemblyInfo divided into GlobalAssemblyInfo file for solution wide infos, and project specific AssemblyInfo files

--- AudioAlign/AudioAlign.Audio/Properties/AssemblyInfo.cs
+++ AudioAlign/AudioAlign.Audio/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("AudioAlign.Audio")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AudioAlign.Audio")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4c2338d7-72c1-4c19-a835-8acb2e6afee5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AudioAlign/GlobalAssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// http://stackoverflow.com/questions/62353/what-are-the-best-practices-for-using-assembly-attributes
+
+[assembly: AssemblyProduct("AudioAlign")]
+[assembly: AssemblyCompany("Mario Guggenberger")]
+[assembly: AssemblyCopyright("Copyright © 2011 Mario Guggenberger")]
+//[assembly: AssemblyTrademark("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0 Alpha")]
