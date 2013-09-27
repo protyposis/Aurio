e7755b5e5ce15ee17cfe8a121bae00626d0a7d24
Mario Guggenberger <mg@protyposis.net>
2013-09-27 09:28:42 +0000

    JIKU dataset sorting by filename timestamps

--- AudioAlign/AudioAlign.Audio/TimeUtil.cs
+++ AudioAlign/AudioAlign.Audio/TimeUtil.cs
@@ -7,11 +7,13 @@ namespace AudioAlign.Audio {
     public static class TimeUtil {
 
         public static readonly long SECS_TO_TICKS;
+        public static readonly long MILLISECS_TO_TICKS;
 
         static TimeUtil() {
             // 1 sec * 1000 = millisecs | * 1000 = microsecs | * 10 = ticks (100-nanosecond units)
             // http://msdn.microsoft.com/en-us/library/zz841zbz.aspx
             SECS_TO_TICKS = 1 * 1000 * 1000 * 10;
+            MILLISECS_TO_TICKS = SECS_TO_TICKS/1000;
         }
 
         public static long TimeSpanToBytes(TimeSpan timeSpan, Streams.AudioProperties audioProperties) {
