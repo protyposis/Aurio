3947ce84b4580f177f2ac14b29cb93cab1f58331
Mario Guggenberger <mg@protyposis.net>
2015-03-03 21:13:34 +0100

    Add FFmpeg test application

---
+++ AudioAlign/AudioAlign.FFmpeg.Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using AudioAlign.Audio.Streams;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AudioAlign.FFmpeg.Test {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length == 0) {
+                Console.WriteLine("no input file specified");
+                return;
+            }
+
+            // TODO read audio from FFmpeg
+            FFmpegReader reader = new FFmpegReader(args[0]);
+
+            int samplesRead;
+            while ((samplesRead = reader.ReadFrame()) > 0) {
+                Console.WriteLine("read " + samplesRead);
+            }
+
+            reader.Dispose();
+
+            //IeeeStream ieee = new IeeeStream(nAudioSource);
+            //NAudioSinkStream nAudioSink = new NAudioSinkStream(ieee);
+            //WaveFileWriter.CreateWaveFile(dlg.FileName + ".processed.wav", nAudioSink);
+
+        }
+    }
+}
