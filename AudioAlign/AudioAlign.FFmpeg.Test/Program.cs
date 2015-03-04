69b4dfcdb26c42e654990378ab3644a8dbffd818
Mario Guggenberger <mg@protyposis.net>
2015-03-04 14:38:20 +0100

    Move the MemorySourceStream to the core streams

--- AudioAlign/AudioAlign.FFmpeg.Test/Program.cs
+++ AudioAlign/AudioAlign.FFmpeg.Test/Program.cs
@@ -66,36 +66,4 @@ namespace AudioAlign.FFmpeg.Test {
 
         }
     }
-
-    class MemorySourceStream : IAudioStream {
-
-        private MemoryStream source;
-        private AudioProperties properties;
-
-        public MemorySourceStream(MemoryStream source, AudioProperties properties) {
-            this.source = source;
-            this.properties = properties;
-        }
-
-        public AudioProperties Properties {
-            get { return properties; }
-        }
-
-        public long Length {
-            get { return source.Length; }
-        }
-
-        public long Position {
-            get { return source.Position; }
-            set { source.Position = value; }
-        }
-
-        public int SampleBlockSize {
-            get { return properties.SampleBlockByteSize; }
-        }
-
-        public int Read(byte[] buffer, int offset, int count) {
-            return source.Read(buffer, offset, count);
-        }
-    }
 }
