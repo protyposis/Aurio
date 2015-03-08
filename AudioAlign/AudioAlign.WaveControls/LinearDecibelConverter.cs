2b77e3246d99120cb97fd93cf291db3c7baae4a2
Mario Guggenberger <mg@protyposis.net>
2015-03-08 17:53:06 +0100

    Improve dB formatting in GUI
    This commit adds custom dB formatting instead of the built in double.toString() formatting, which is still used except for the special case negative infinity, in which the infinity character is now used. This looks much cleaner in the GUI.

--- AudioAlign/AudioAlign.WaveControls/LinearDecibelConverter.cs
+++ AudioAlign/AudioAlign.WaveControls/LinearDecibelConverter.cs
@@ -27,4 +27,63 @@ namespace AudioAlign.WaveControls {
 
         #endregion
     }
+
+    public class DecibelStringConverter : IValueConverter {
+
+        private const string NEG_INFINITY = "-∞";
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            double dB = (double)value;
+
+            // double to string
+            if (dB == double.NegativeInfinity) {
+                return NEG_INFINITY;
+            }
+            else {
+                return dB.ToString("0.0");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            double dB = double.NaN;
+
+            // string to double
+            if (value != null && value is string) {
+                if ((string)value == NEG_INFINITY) {
+                    dB = double.NegativeInfinity;
+                }
+                else {
+                    dB = double.Parse((string)value);
+                }
+            }
+
+            return dB;
+
+        }
+
+        #endregion
+    }
+
+    public class LinearDecibelStringConverter : IValueConverter {
+
+        private static readonly LinearDecibelConverter linearDecibelConverter = new LinearDecibelConverter();
+        private static readonly DecibelStringConverter decibelStringConverter = new DecibelStringConverter();
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            double dB = (double)linearDecibelConverter.Convert(value, targetType, parameter, culture);
+            return decibelStringConverter.Convert(dB, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            double dB = (double)decibelStringConverter.ConvertBack(value, targetType, parameter, culture);
+            return linearDecibelConverter.ConvertBack(dB, targetType, parameter, culture);
+
+        }
+
+        #endregion
+    }
 }
