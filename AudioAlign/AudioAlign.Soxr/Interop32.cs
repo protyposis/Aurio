b3a04618416fbebede8207946dc61a0eeca03d67
Mario Guggenberger <mg@protyposis.net>
2015-03-30 20:32:51 +0200

    Instance creation and error reading made working

--- AudioAlign/AudioAlign.Soxr/Interop32.cs
+++ AudioAlign/AudioAlign.Soxr/Interop32.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
+using StringPtr = System.IntPtr;
 using SoxrInstance = System.IntPtr;
 using SoxrError = System.IntPtr;
 using SoxrIoSpec = System.IntPtr;
@@ -11,49 +12,49 @@ using SoxrQualitySpec = System.IntPtr;
 using SoxrRuntimeSpec = System.IntPtr;
 
 namespace AudioAlign.Soxr {
-    [SuppressUnmanagedCodeSecurity]
+    //[SuppressUnmanagedCodeSecurity]
     internal unsafe class Interop32 {
 
         private const string SOXRLIB = "soxr.x86.dll";
         private const CallingConvention CC = CallingConvention.Cdecl;
 
         [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern string soxr_version();
+        public static extern StringPtr soxr_version();
 
         [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern SoxrInstance soxr_create(double input_rate, double output_rate, uint num_channels, 
-            SoxrError error, SoxrIoSpec io_spec, SoxrQualitySpec quality_spec, SoxrRuntimeSpec runtime_spec);
+        public static extern SoxrInstance soxr_create(double input_rate, double output_rate, uint num_channels,
+            out SoxrError error, SoxrIoSpec io_spec, SoxrQualitySpec quality_spec, SoxrRuntimeSpec runtime_spec);
         
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern SoxrError soxr_process(SoxrInstance resampler, 
-            byte *input, uint ilen, out uint idone,
-            byte *output, uint olen, out uint odone);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern SoxrError soxr_process(SoxrInstance resampler, 
+        //    byte *input, uint ilen, out uint idone,
+        //    byte *output, uint olen, out uint odone);
 
         [DllImport(SOXRLIB, CallingConvention = CC)]
         public static extern SoxrError soxr_error(SoxrInstance resampler);
 
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern double soxr_delay(SoxrInstance resampler);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern double soxr_delay(SoxrInstance resampler);
 
         [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern string soxr_engine(SoxrInstance resampler);
+        public static extern StringPtr soxr_engine(SoxrInstance resampler);
 
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern SoxrError soxr_clear(SoxrInstance resampler);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern SoxrError soxr_clear(SoxrInstance resampler);
 
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern void soxr_delete(SoxrInstance resampler);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern void soxr_delete(SoxrInstance resampler);
 
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern SoxrError soxr_set_io_ratio(SoxrInstance resampler, double io_ratio, uint slew_len);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern SoxrError soxr_set_io_ratio(SoxrInstance resampler, double io_ratio, uint slew_len);
 
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern SoxrQualitySpec soxr_quality_spec(QualityRecipe recipe, QualityFlags flags);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern SoxrQualitySpec soxr_quality_spec(QualityRecipe recipe, QualityFlags flags);
 
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern SoxrRuntimeSpec soxr_runtime_spec(uint num_threads);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern SoxrRuntimeSpec soxr_runtime_spec(uint num_threads);
 
-        [DllImport(SOXRLIB, CallingConvention = CC)]
-        public static extern SoxrIoSpec soxr_io_spec(Datatype itype, Datatype otype);
+        //[DllImport(SOXRLIB, CallingConvention = CC)]
+        //public static extern SoxrIoSpec soxr_io_spec(Datatype itype, Datatype otype);
     }
 }
--- AudioAlign/AudioAlign.Soxr/SoxResampler.cs
+++ AudioAlign/AudioAlign.Soxr/SoxResampler.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using StringPtr = System.IntPtr;
 using SoxrInstance = System.IntPtr;
 using SoxrError = System.IntPtr;
 using SoxrIoSpec = System.IntPtr;
 using SoxrQualitySpec = System.IntPtr;
 using SoxrRuntimeSpec = System.IntPtr;
+using System.Runtime.InteropServices;
 
 namespace AudioAlign.Soxr {
     public class SoxResampler {
@@ -14,11 +16,42 @@ namespace AudioAlign.Soxr {
         private SoxrInstance soxr;
 
         public SoxResampler(double inputRate, double outputRate, int channels) {
-            soxr = InteropWrapper.soxr_create(inputRate, outputRate, (uint)channels, SoxrError.Zero, SoxrIoSpec.Zero , SoxrQualitySpec.Zero, SoxrRuntimeSpec.Zero);
+            SoxrError error = SoxrError.Zero;
+            
+            soxr = InteropWrapper.soxr_create(inputRate, outputRate, (uint)channels, 
+                out error, SoxrIoSpec.Zero, SoxrQualitySpec.Zero, SoxrRuntimeSpec.Zero);
+            
+            if (error != SoxrError.Zero) {
+                throw new SoxrException(GetError(error));
+            }
         }
 
         public string Version {
-            get { return InteropWrapper.soxr_version(); }
+            get { 
+                StringPtr ptr = InteropWrapper.soxr_version();
+                return Marshal.PtrToStringAnsi(ptr);
+            }
+        }
+
+        public string Engine {
+            get {
+                StringPtr ptr = InteropWrapper.soxr_engine(soxr);
+                return Marshal.PtrToStringAnsi(ptr);
+            }
+        }
+
+        private string GetError(SoxrError error) {
+            if (error == SoxrError.Zero) {
+                return null;
+            }
+            else {
+                return Marshal.PtrToStringAnsi(error);
+            }
+        }
+
+        public string GetError() {
+            SoxrError ptr = InteropWrapper.soxr_error(soxr);
+            return GetError(ptr);
         }
     }
 }
