862e0a0d0c957e5c3e29ccea9278ca0cf6d7cb80
Mario Guggenberger <mg@protyposis.net>
2015-03-30 19:14:45 +0200

    Add enums and untested interop methods

---
+++ AudioAlign/AudioAlign.Soxr/Datatype.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AudioAlign.Soxr {
+    enum Datatype {
+        SOXR_FLOAT32_I = 0,
+        SOXR_FLOAT64_I,
+        SOXR_INT32_I,
+        SOXR_INT16_I,
+
+        SOXR_FLOAT32_S = 4,
+        SOXR_FLOAT64_S,
+        SOXR_INT32_S,
+        SOXR_INT16_S
+    }
+}
