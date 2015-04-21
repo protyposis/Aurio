4cad584371a45e42ec0e5f0b04bdde4eed567800
Mario Guggenberger <mg@protyposis.net>
2015-04-21 18:43:30 +0200

    Add basic matching

--- AudioAlign/AudioAlign.Audio/Matching/Wang2003/FingerprintHash.cs
+++ AudioAlign/AudioAlign.Audio/Matching/Wang2003/FingerprintHash.cs
@@ -15,5 +15,25 @@ namespace AudioAlign.Audio.Matching.Wang2003 {
         public UInt32 Value { 
             get { return this.value; } 
         }
+
+        public override bool Equals(object obj) {
+            return this.value.Equals(((FingerprintHash)obj).value);
+        }
+
+        public override int GetHashCode() {
+            return this.value.GetHashCode();
+        }
+
+        public override string ToString() {
+            return "FingerprintHash {" + Convert.ToString(this.value, 2).PadLeft(32, '0') + " (" + this.value + ")}";
+        }
+
+        public static bool operator !=(FingerprintHash a, FingerprintHash b) {
+            return a.value != b.value;
+        }
+
+        public static bool operator ==(FingerprintHash a, FingerprintHash b) {
+            return a.value == b.value;
+        }
     }
 }
--- AudioAlign/AudioAlign.Audio/Matching/Wang2003/FingerprintStore.cs
+++ AudioAlign/AudioAlign.Audio/Matching/Wang2003/FingerprintStore.cs
@@ -45,7 +45,55 @@ namespace AudioAlign.Audio.Matching.Wang2003 {
             List<Match> matches = new List<Match>();
             List<FingerprintHashLookupEntry> entries = collisionMap.GetValues(hash);
 
-            //
+            for (int x = 0; x < entries.Count; x++) {
+                FingerprintHashLookupEntry entry1 = entries[x];
+                for (int y = x; y < entries.Count; y++) {
+                    FingerprintHashLookupEntry entry2 = entries[y];
+                    if (entry1.AudioTrack != entry2.AudioTrack) { // don't compare tracks with themselves
+
+                        var store1 = store[entry1.AudioTrack];
+                        var store2 = store[entry2.AudioTrack];
+                        List<FingerprintHash> hashes1 = store1.hashes;
+                        List<FingerprintHash> hashes2 = store2.hashes;
+                        int index1 = entry1.Index;
+                        int index2 = entry2.Index;
+                        TrackStore.IndexEntry indexEntry1, indexEntry2;
+                        int numTried = 0;
+                        int numMatched = 0;
+
+                        while(store1.index.ContainsKey(index1) && store2.index.ContainsKey(index2) && numTried < 200) {
+                            indexEntry1 = store1.index[index1];
+                            indexEntry2 = store2.index[index2];
+
+                            int ii = 0;
+                            for (int i = indexEntry1.index; i < indexEntry1.EndIndex; i++) {
+                                for (int j = indexEntry2.index + ii; j < indexEntry2.EndIndex; j++) {
+                                    if (hashes1[i] == hashes2[j]) {
+                                        numMatched++;
+                                    }
+                                    else {
+                                        numTried++;
+                                    }
+                                }
+                                ii++;
+                            }
+                            index1++;
+                            index2++;
+                        }
+
+                        if (numMatched > 50 && numTried > 100) {
+                            matches.Add(new Match {
+                                Similarity = 1 / numTried * numMatched,
+                                Track1 = entry1.AudioTrack,
+                                Track1Time = FingerprintGenerator.FingerprintHashIndexToTimeSpan(entry1.Index),
+                                Track2 = entry2.AudioTrack,
+                                Track2Time = FingerprintGenerator.FingerprintHashIndexToTimeSpan(entry2.Index),
+                                Source = "FP-W03"
+                            });
+                        }
+                    }
+                }
+            }
 
             return matches;
         }
@@ -59,6 +107,8 @@ namespace AudioAlign.Audio.Matching.Wang2003 {
             foreach (FingerprintHash hash in collidingKeys) {
                 matches.AddRange(FindMatches(hash));
             }
+            Debug.WriteLine("{0} matches", matches.Count);
+
             return matches;
         }
 
@@ -73,6 +123,10 @@ namespace AudioAlign.Audio.Matching.Wang2003 {
                 public IndexEntry(int index, int length) {
                     this.index = index;
                     this.length = length;
+                }
+
+                public int EndIndex {
+                    get { return index + length; }
                 }
             }
 
