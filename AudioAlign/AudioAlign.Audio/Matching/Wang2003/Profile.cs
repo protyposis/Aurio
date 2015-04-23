623e5a51d9cdebb8e190c03c3e4187666e2ed64b
Mario Guggenberger <mg@protyposis.net>
2015-04-23 10:23:29 +0200

    Move threshold to the config profile

--- AudioAlign/AudioAlign.Audio/Matching/Wang2003/Profile.cs
+++ AudioAlign/AudioAlign.Audio/Matching/Wang2003/Profile.cs
@@ -12,6 +12,28 @@ namespace AudioAlign.Audio.Matching.Wang2003 {
     /// </summary>
     public class Profile {
 
+        public interface IThreshold {
+            /// <summary>
+            /// Returns, for a given x (time), a threshold value in the range of [0,1].
+            /// </summary>
+            /// <param name="x">the time instant to calculate the threshold for</param>
+            /// <returns>the threshold for the given time instant</returns>
+            double Calculate(double x);
+        }
+
+        /// <summary>
+        /// An exponentially decaying threshold in the form of y=b^x.
+        /// </summary>
+        public class ExponentialDecayThreshold : IThreshold {
+            public double Base { get; set; }
+            public double WidthScale { get; set; }
+            public double Height { get; set; }
+
+            public double Calculate(double x) {
+                return Math.Pow(Base, x / WidthScale) * Height;
+            }
+        }
+
         public Profile() {
             SamplingRate = 11025;
             WindowSize = 512;
@@ -23,6 +45,22 @@ namespace AudioAlign.Audio.Matching.Wang2003 {
             TargetZoneDistance = 2;
             TargetZoneLength = 30;
             TargetZoneWidth = 63;
+
+            double framesPerSecond = (double)SamplingRate / HopSize;
+            MatchingMinFrames = 10;
+            MatchingMaxFrames = (int)(framesPerSecond * 30);
+
+            var threshold = new ExponentialDecayThreshold {
+                Base = 0.5,
+                WidthScale = 2,
+                Height = 0.3
+            };
+            ThresholdAccept = threshold;
+            ThresholdReject = new ExponentialDecayThreshold {
+                Base = threshold.Base,
+                WidthScale = threshold.WidthScale,
+                Height = threshold.Height / 6
+            };
         }
 
         /// <summary>
@@ -79,5 +117,25 @@ namespace AudioAlign.Audio.Matching.Wang2003 {
         /// The spectral width in peaks of the target peak zone.
         /// </summary>
         public int TargetZoneWidth { get; set; }
+
+        /// <summary>
+        /// The minimum length in frames to classify a match.
+        /// </summary>
+        public int MatchingMinFrames { get; set; }
+
+        /// <summary>
+        /// The maximum number of frames to scan for a match.
+        /// </summary>
+        public int MatchingMaxFrames { get; set; }
+
+        /// <summary>
+        /// The threshold that a match candidate's rate needs to exceed to be classified as a match.
+        /// </summary>
+        public IThreshold ThresholdAccept { get; set; }
+
+        /// <summary>
+        /// The threshold that rejects a match candidate and stops the matching process if it's matching rate drops below.
+        /// </summary>
+        public IThreshold ThresholdReject { get; set; }
     }
 }
