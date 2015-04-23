c4829cc4daf49c579a13cb1b3e98986d72648218
Mario Guggenberger <mg@protyposis.net>
2015-04-23 19:41:09 +0200

    Move CFA feature to the library

--- AudioAlign/MusicDetector/Program.cs
+++ AudioAlign/MusicDetector/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using MusicDetector.ContinuousFrequencyActivation;
 using AudioAlign.Audio.Project;
+using AudioAlign.Audio.Features.ContinuousFrequencyActivation;
 
 namespace MusicDetector {
     class Program {
