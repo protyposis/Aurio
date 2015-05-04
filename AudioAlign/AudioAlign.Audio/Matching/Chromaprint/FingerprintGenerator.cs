675d355e367e3ce943b81d140a5aff533027e00c
Mario Guggenberger <mg@protyposis.net>
2015-05-04 22:17:27 +0200

    Base classes

---
+++ AudioAlign/AudioAlign.Audio/Matching/Chromaprint/FingerprintGenerator.cs
@@ -0,0 +1,29 @@
+﻿using AudioAlign.Audio.Project;
+using AudioAlign.Audio.Streams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AudioAlign.Audio.Matching.Chromaprint {
+    /// <summary>
+    /// Chromaprint fingerprint generator as described in:
+    /// - https://oxygene.sk/2010/07/introducing-chromaprint/
+    /// - https://oxygene.sk/2011/01/how-does-chromaprint-work/
+    /// - https://bitbucket.org/acoustid/chromaprint/
+    /// </summary>
+    public class FingerprintGenerator {
+
+        private Profile profile;
+
+        public FingerprintGenerator(Profile profile) {
+            this.profile = profile;
+        }
+
+        public void Generate(AudioTrack track) {
+            IAudioStream audioStream = new ResamplingStream(
+                new MonoStream(AudioStreamFactory.FromFileInfoIeee32(track.FileInfo)),
+                ResamplingQuality.Medium, profile.SamplingRate);
+        }
+    }
+}
---
+++ AudioAlign/AudioAlign.Audio/Matching/Chromaprint/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AudioAlign.Audio.Matching.Chromaprint {
+    public class Profile {
+
+        public Profile() {
+            SamplingRate = 11025;
+        }
+
+        public int SamplingRate { get; set; }
+    }
+}
