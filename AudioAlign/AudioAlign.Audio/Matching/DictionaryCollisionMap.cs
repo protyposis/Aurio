a2341b73bed30ef0bd91a22e2320e35846b14184
Mario Guggenberger <mg@protyposis.net>
2015-05-06 21:28:41 +0200

    Unified collision map implementations

--- AudioAlign/AudioAlign.Audio/Matching/DictionaryCollisionMap.cs
+++ AudioAlign/AudioAlign.Audio/Matching/DictionaryCollisionMap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AudioAlign.Audio.Matching.Chromaprint {
+namespace AudioAlign.Audio.Matching {
     class DictionaryCollisionMap : IFingerprintCollisionMap {
 
         private Dictionary<SubFingerprint, List<SubFingerprintLookupEntry>> lookupTable;
