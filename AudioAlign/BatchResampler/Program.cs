c48c0885ed6e80d9276b70af24f2dcdc987c052f
Mario Guggenberger <mg@protyposis.net>
2015-05-10 19:37:31 +0200

    Improve usability of the batch resampler

--- AudioAlign/BatchResampler/Program.cs
+++ AudioAlign/BatchResampler/Program.cs
@@ -2,6 +2,7 @@
 using AudioAlign.Audio.Streams;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,19 +12,47 @@ namespace BatchResampler
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // read config file
-            Dictionary<string, double> mapping = ReadConfig(args[0]);
+            try
+            {
+                // read config file
+                Dictionary<string, double> mapping = ReadConfig(args[0]);
 
-            // read input dir
-            DirectoryInfo indir = new DirectoryInfo(args[1]);
+                // read input dir
+                DirectoryInfo indir = new DirectoryInfo(args[1]);
 
-            // read output dir
-            DirectoryInfo outdir = new DirectoryInfo(args[2]);
-            if (!outdir.Exists)
-                outdir.Create();
+                // read output dir
+                DirectoryInfo outdir = new DirectoryInfo(args[2]);
+                if (!outdir.Exists)
+                    outdir.Create();
 
+                Process(mapping, indir, outdir);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Batch Resampling Tool to change the playback speed of audio files");
+                Console.WriteLine();
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine();
+
+                var info = "Usage: BatchResampler mappingFile inputDirectory outputDirectory" + Environment.NewLine +
+                           Environment.NewLine +
+                           "The mappingFile is a text file with at least one line of the pattern 'filenamePattern;resamplingFactor' (without quotes), " +
+                           "where the filenamePattern can be a filename or a filename wildcard expression to be matched in the input directory, and the " +
+                           "resamplingFactor is a floating point value specifying the input:output resampling ratio with which the files will be written" +
+                           "to the output directory. The simplest mappingFile would be '*;1', which means that all files in the input directory will be" +
+                           "resampled with a factor of 1.0 (i.e. no resampling at all) and written to the output directory. The nominal sampling rate of a file" +
+                           "stays untouched, leading to altered playback speeds." + Environment.NewLine +
+                           "A mappingFile with the content '*.wav;0.5' speeds up all wave audio files to 200% playback speed because they are getting resampled " +
+                           "to half of their effective sampling rate while the nominal playback sample rate stays the same, cutting their playback duration in half.";
+
+                Console.WriteLine(info);
+            }
+        }
+
+        static void Process(Dictionary<string, double> mapping, DirectoryInfo indir, DirectoryInfo outdir)
+        {
             Dictionary<FileInfo, double> fileMapping = new Dictionary<FileInfo, double>();
 
             foreach (string fileNamePattern in mapping.Keys)
@@ -41,7 +70,7 @@ namespace BatchResampler
 
                 if (outputFileInfo.Exists)
                 {
-                    Console.WriteLine(fileInfo.Name + " SKIP");
+                    Console.WriteLine(fileInfo.Name + " SKIP (file already existing)");
                     return;
                 }
 
@@ -76,7 +105,7 @@ namespace BatchResampler
                 string line;
                 while((line = reader.ReadLine()) != null) {
                     string[] parts = line.Split(';');
-                    mapping.Add(parts[0], Double.Parse(parts[1]));
+                    mapping.Add(parts[0], Double.Parse(parts[1], CultureInfo.InvariantCulture.NumberFormat));
                 }
             }
 
