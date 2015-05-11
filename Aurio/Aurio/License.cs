ea79343e7dfd7286001da684a552bf021d0518e2
Mario Guggenberger <mg@protyposis.net>
2015-05-11 17:11:39 +0200

    Add a convenient license info getter method
    This should be used in applications using this library to get the licensing info.

---
+++ Aurio/Aurio/License.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Aurio {
+    public static class License {
+        public static string Info {
+            get { return ReadEmbeddedResourceFileText("Aurio.licenseinfo.txt"); }
+        }
+
+        private static string ReadEmbeddedResourceFileText(string filename) {
+            string text = String.Empty;
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(filename)) {
+                using (StreamReader reader = new StreamReader(stream)) {
+                    text = reader.ReadToEnd();
+                }
+            }
+            return text;
+        }
+    }
+}
