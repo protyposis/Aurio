e9c2fbb47e41cf4765f399571ea328a2424ca192
Mario Guggenberger <mg@protyposis.net>
2016-03-01 12:30:07 +0100

    Update copyright year

--- Aurio/Aurio.FFmpeg/Properties/AssemblyInfo.cs
+++ Aurio/Aurio.FFmpeg/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Aurio FFmpeg Interface")]
-[assembly: AssemblyCopyright("Copyright © 2015 Mario Guggenberger")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016 Mario Guggenberger")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
--- Aurio/Aurio/Properties/AssemblyInfo.cs
+++ Aurio/Aurio/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Aurio Core Library")]
-[assembly: AssemblyCopyright("Copyright © 2010-2015 Mario Guggenberger")]
+[assembly: AssemblyCopyright("Copyright © 2010-2016 Mario Guggenberger")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
