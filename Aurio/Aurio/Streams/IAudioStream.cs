6945c06d56bc1b727ebc46d63cb3905bfd72e7e8
Mario Guggenberger <mg@protyposis.net>
2016-03-01 14:33:07 +0100

    Implement IDisposable in audio streams

--- Aurio/Aurio/Streams/IAudioStream.cs
+++ Aurio/Aurio/Streams/IAudioStream.cs
@@ -1,6 +1,6 @@
 ﻿// 
 // Aurio: Audio Processing, Analysis and Retrieval Library
-// Copyright (C) 2010-2015  Mario Guggenberger <mg@protyposis.net>
+// Copyright (C) 2010-2016  Mario Guggenberger <mg@protyposis.net>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +25,7 @@ namespace Aurio.Streams {
     /// <summary>
     /// An audio stream.
     /// </summary>
-    public interface IAudioStream {
+    public interface IAudioStream : IDisposable {
         /// <summary>
         /// Returns the properties of the audio stream (e.g. sample rate, bit rate, number of channels).
         /// </summary>
--- Aurio/Aurio/Streams/MemorySourceStream.cs
+++ Aurio/Aurio/Streams/MemorySourceStream.cs
@@ -1,6 +1,6 @@
 ﻿// 
 // Aurio: Audio Processing, Analysis and Retrieval Library
-// Copyright (C) 2010-2015  Mario Guggenberger <mg@protyposis.net>
+// Copyright (C) 2010-2016  Mario Guggenberger <mg@protyposis.net>
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -60,5 +61,23 @@ namespace Aurio.Streams {
         public void Close() {
             source.Close();
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    Close();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+        }
+        #endregion
     }
 }
