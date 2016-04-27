cc891057627c6f2aef391a3c3d7f9329a7a0b00c
Mario Guggenberger <mg@protyposis.net>
2016-04-27 21:11:07 +0200

    Add extension methods for TimeSpan division and multiplication

---
+++ Aurio/Aurio/TimeSpanExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aurio {
+    public static class TimeSpanExtensions {
+
+        public static TimeSpan Divide(this TimeSpan ts, double divisor) {
+            return new TimeSpan((long)(ts.Ticks / divisor));
+        }
+
+        public static TimeSpan Multiply(this TimeSpan ts, double multiplicand) {
+            return new TimeSpan((long)(ts.Ticks * multiplicand));
+        }
+    }
+}
