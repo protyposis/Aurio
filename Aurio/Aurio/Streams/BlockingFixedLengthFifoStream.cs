109eda723035e18700c55d05649c672d2249005f
Mario Guggenberger <mg@protyposis.net>
2018-03-21 17:06:56 +0100

    Return infinite length until the end of input is signaled
    Avoids incorrect end of stream detections when the length is compared to the position.

--- Aurio/Aurio/Streams/BlockingFixedLengthFifoStream.cs
+++ Aurio/Aurio/Streams/BlockingFixedLengthFifoStream.cs
@@ -19,40 +19,97 @@ namespace Aurio.Streams
     public class BlockingFixedLengthFifoStream : CircularMemoryWriterStream
     {
         private long _readPosition;
+        private bool _endOfInput;
 
         public BlockingFixedLengthFifoStream(AudioProperties properties, int capacity) : base(properties, capacity)
         {
             _readPosition = 0;
+            _endOfInput = false;
         }
 
+        /// <summary>
+        /// The length of the stream. Since the length of this stream is unsually not known and only
+        /// the most recent part is buffered, this returns infinity (respectively the max long value)
+        /// until the end of input has been signalled, after which the actual length will be returned.
+        /// </summary>
+        /// <see cref="EndOfInputSignalled"/>
         public override long Length
         {
             [MethodImpl(MethodImplOptions.Synchronized)]
             get
             {
-                return base.Length;
+                if (_endOfInput)
+                {
+                    // When EOI has been signalled, we return the actual length of this stream
+                    // to allow consumers real all contents by comparing the the length with the 
+                    // position (the usual pattern for stream reading).
+                    return base.Length;
+                } else
+                {
+                    // Without the EOI signal, we return the maximum possible length (meaning
+                    // infinity) to tell a consumer that the stream is potentially infinite and
+                    // a comparison of length and position never allows the conclusion that the
+                    // end of the stream has been reached.
+                    return long.MaxValue;
+                }
             }
         }
 
+        /// <summary>
+        /// By convention of the stream interface, there is normally only one position which is both
+        /// the read and write position. This stream exposes read and write positions separately because
+        /// writes are appended at the end fo the stream while reads are done from the beginning of 
+        /// the stream (FIFO approach).
+        /// 
+        /// This property exposes the read position, because this is the interesting position that must 
+        /// be used to calculate the amount of remaining data by comparing the position with the length.
+        /// 
+        /// Comparing the read with the write position allows to determine the buffer level, i.e. the amount
+        /// of data that has not been consumed yet, independently from how much data the buffer holds in total.
+        /// </summary>
+        /// <see cref="WritePosition"/>
         public override long Position
         {
             [MethodImpl(MethodImplOptions.Synchronized)]
-            get
-            {
-                return base.Position;
-            }
+            get => _readPosition;
             [MethodImpl(MethodImplOptions.Synchronized)]
-            set
-            {
-                base.Position = value;
-            }
+            set => throw new InvalidOperationException("Cannot set the read position");
         }
 
-        public long ReadPosition
+        public long WritePosition
         {
-            get { return _readPosition; }
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            get => base.Position;
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            set => base.Position = value;
         }
 
+        /// <summary>
+        /// Signals that no more data will be written to the stream and it will come to and end.
+        /// After the signal, no more data will be allowed to be written but all buffered data
+        /// can still be consumed.
+        /// </summary>
+        /// <see cref="EndOfInputSignalled"/>
+        public void SignalEndOfInput()
+        {
+            _endOfInput = true;
+        }
+
+        /// <summary>
+        /// Tells if the end of input has been signalled.
+        /// </summary>
+        /// <see cref="SignalEndOfInput"/>
+        public bool EndOfInputSignalled
+        {
+            get => _endOfInput;
+        }
+
+        /// <summary>
+        /// Reads data from the stream, i.e. from the beginning of the FIFO buffer. This method blocks
+        /// until additional data is written to the stream and becomes available, or the end of input
+        /// has been signalled.
+        /// </summary>
+        /// <see cref="EndOfInputSignalled"/> 
         [MethodImpl(MethodImplOptions.Synchronized)]
         public override int Read(byte[] buffer, int offset, int count)
         {
@@ -64,6 +121,12 @@ namespace Aurio.Streams
             long bytesAvailable;
             while ((bytesAvailable = base.Position - _readPosition) <= 0)
             {
+                if (_endOfInput)
+                {
+                    // At the EOI we don't block because no more data will become available.
+                    return 0;
+                }
+
                 // Debug.WriteLine("Waiting for data... ({0} required, {1} available)", count, bytesAvailable);
                 Monitor.Wait(this);
             }
@@ -86,6 +149,11 @@ namespace Aurio.Streams
         [MethodImpl(MethodImplOptions.Synchronized)]
         public override void Write(byte[] buffer, int offset, int count)
         {
+            if (_endOfInput)
+            {
+                throw new Exception("End of input has been signalled, no more data can be written");
+            }
+
             // Write data
             // The read method makes sure that the stream is always at the write position when 
             // writing so we don't need to take care of that here
