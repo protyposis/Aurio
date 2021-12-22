8fa369b36cba56f2a78190165e7281d50adcd578
Mario Guggenberger <mg@protyposis.net>
2021-12-22 15:52:46 +0100

    refactor: expose peak-related structs

--- Aurio/Aurio/Matching/Wang2003/FingerprintGenerator.cs
+++ Aurio/Aurio/Matching/Wang2003/FingerprintGenerator.cs
@@ -152,20 +152,14 @@ namespace Aurio.Matching.Wang2003
 
                 if (FrameProcessed != null)
                 {
-                    // Mark peaks as 0dB for spectrogram display purposes
-                    foreach (var peak in peaks)
-                    {
-                        spectrum[peak.Index] = 0;
-                        spectrumResidual[peak.Index] = 0;
-                    }
-
                     FrameProcessed(this, new FrameProcessedEventArgs
                     {
                         AudioTrack = track,
                         Index = index,
                         Indices = indices,
                         Spectrum = spectrum,
-                        SpectrumResidual = spectrumResidual
+                        SpectrumResidual = spectrumResidual,
+                        Peaks = peaks,
                     });
                 }
 
@@ -373,54 +367,6 @@ namespace Aurio.Matching.Wang2003
         public static Profile[] GetProfiles()
         {
             return new Profile[] { new DefaultProfile() };
-        }
-
-        [DebuggerDisplay("{index}/{value}")]
-        private struct Peak
-        {
-
-            private int index;
-            private float value;
-
-            public Peak(int index, float value)
-            {
-                this.index = index;
-                this.value = value;
-            }
-            public int Index { get { return index; } }
-            public float Value { get { return value; } }
-        }
-
-        [DebuggerDisplay("{Index}:{Peak1.Index} --({Distance})--> {Peak2.Index}")]
-        private struct PeakPair
-        {
-            public int Index { get; set; }
-            public Peak Peak1 { get; set; }
-            public Peak Peak2 { get; set; }
-            public int Distance { get; set; }
-            public float AverageEnergy { get { return (Peak1.Value + Peak2.Value) / 2; } }
-
-            public static uint PeakPairToHash(PeakPair pp)
-            {
-                // Put frequency bins and the distance each in one byte. The actual quantization
-                // is configured through the parameters, e.g. the FFT window size determines the
-                // number of frequency bins, and the size of the target zone determines the max
-                // distance. Their max size can be anywhere in the range of a byte. if it should be 
-                // higher, a quantization step must be introduced (which will basically be a division).
-                return (uint)((byte)pp.Peak1.Index << 16 | (byte)pp.Peak2.Index << 8 | (byte)pp.Distance);
-            }
-
-            public static PeakPair HashToPeakPair(uint hash, int index)
-            {
-                // The inverse operation of the function above.
-                return new PeakPair
-                {
-                    Index = index,
-                    Peak1 = new Peak((int)(hash >> 16 & 0xFF), 0),
-                    Peak2 = new Peak((int)(hash >> 8 & 0xFF), 0),
-                    Distance = (int)(hash & 0xFF)
-                };
-            }
         }
 
         /// <summary>
