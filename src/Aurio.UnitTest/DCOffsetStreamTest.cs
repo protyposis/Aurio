46207bf682bc2c2ed9d96df06432d7461e5001be
Mario Guggenberger <mg@protyposis.net>
2023-12-06 14:03:27 +0100

    feat: `DCOffsetStream`

---
+++ src/Aurio.UnitTest/DCOffsetStreamTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aurio.Streams;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aurio.UnitTest
+{
+    [TestClass]
+    public class DCOffsetStreamTest
+    {
+        [TestMethod]
+        public void DCOffsetApplied()
+        {
+            var offset = 0.2f;
+            var input = new float[] { -1.3f, -0.5f, 0, 0.1f, 1f };
+            var s = new DCOffsetStream(new MemorySourceStream(input, 100, 1), offset);
+
+            var output = StreamUtil.ReadFloats(s, input.Length);
+
+            CollectionAssert.AreEqual(input.Select(x => x + offset).ToArray(), output);
+        }
+    }
+}
