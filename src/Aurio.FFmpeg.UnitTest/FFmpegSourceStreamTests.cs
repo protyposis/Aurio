010eb87ee80dfaeb7fb417eb20e7cce6d7c9de1c
Mario Guggenberger <mg@protyposis.net>
2024-01-26 19:55:02 +0100

    Add separate Unix tests

--- src/Aurio.FFmpeg.UnitTest/FFmpegSourceStreamTests.cs
+++ src/Aurio.FFmpeg.UnitTest/FFmpegSourceStreamTests.cs
@@ -17,9 +17,11 @@ namespace Aurio.FFmpeg.UnitTest
             Assert.Throws<ArgumentNullException>(act);
         }
 
-        [Fact]
-        public void SuggestWaveProxyFileInfo_WithoutDirectory()
+        [SkippableFact]
+        public void SuggestWaveProxyFileInfo_WithoutDirectory_Windows()
         {
+            Skip.IfNot(OperatingSystem.IsWindows());
+
             var fileInfo = new FileInfo("X:\\folder\\file.wav");
 
             var proxyFileInfo = FFmpegSourceStream.SuggestWaveProxyFileInfo(fileInfo);
@@ -27,9 +29,23 @@ namespace Aurio.FFmpeg.UnitTest
             Assert.Equal("X:\\folder\\file.wav.ffproxy.wav", proxyFileInfo.FullName);
         }
 
-        [Fact]
-        public void SuggestWaveProxyFileInfo_WithDirectory()
+        [SkippableFact]
+        public void SuggestWaveProxyFileInfo_WithoutDirectory_Unix()
         {
+            Skip.If(OperatingSystem.IsWindows());
+
+            var fileInfo = new FileInfo("/folder/file.wav");
+
+            var proxyFileInfo = FFmpegSourceStream.SuggestWaveProxyFileInfo(fileInfo);
+
+            Assert.Equal("/folder/file.wav.ffproxy.wav", proxyFileInfo.FullName);
+        }
+
+        [SkippableFact]
+        public void SuggestWaveProxyFileInfo_WithDirectory_Windows()
+        {
+            Skip.IfNot(OperatingSystem.IsWindows());
+
             var fileInfo = new FileInfo("X:\\folder\\file.wav");
             var directoryInfo = new DirectoryInfo("Y:\\temp\\dir");
 
@@ -40,6 +56,25 @@ namespace Aurio.FFmpeg.UnitTest
 
             Assert.Equal(
                 "Y:\\temp\\dir\\87c18cf1c8d8e07552df4ecc1ef629995fca9c59ad47ccf7eb4816de33590af7.ffproxy.wav",
+                proxyFileInfo.FullName
+            );
+        }
+
+        [SkippableFact]
+        public void SuggestWaveProxyFileInfo_WithDirectory_Unix()
+        {
+            Skip.If(OperatingSystem.IsWindows());
+
+            var fileInfo = new FileInfo("/folder/file.wav");
+            var directoryInfo = new DirectoryInfo("/temp/dir");
+
+            var proxyFileInfo = FFmpegSourceStream.SuggestWaveProxyFileInfo(
+                fileInfo,
+                directoryInfo
+            );
+
+            Assert.Equal(
+                "/temp/dir/e2c5b9282d156c5bdbf95639ca2ce2516b096e4828b7aa16620cb317cc90b3d9.ffproxy.wav",
                 proxyFileInfo.FullName
             );
         }
